180b89c658e302dc84d8cd10062343a6ec51ce38
Jimmy <skylinezum@users.noreply.github.com>
2022-07-29 17:25:12 +0800

    use string comparison for secret

--- packages/app-store/dbee_data/api/middleware/useSecret.ts
+++ packages/app-store/dbee_data/api/middleware/useSecret.ts
@@ -1,13 +1,15 @@
-import bcrypt from 'bcrypt';
+//import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
 
 /**
  * authenticate from backend for private routes
  * bearer token in header
  */
-const useSecret = async (req: Request, res: Response, next: () => void) => {
+const useSecret = (req: Request, res: Response, next: () => void) => {
   const secret = req.header('authorization')?.split(' ')[1] || '';
-  const isAuth = await bcrypt.compare(secret, process.env.DBEE_SECRET_HASH || '');
+  //const isAuth = await bcrypt.compare(secret, process.env.DBEE_SECRET_HASH || '');
+  const isAuth = secret === process.env.DBEE_SECRET_HASH || '';
+  console.log('isAuth', isAuth);
   if (!isAuth) {
     res.sendStatus(401);
     return;
