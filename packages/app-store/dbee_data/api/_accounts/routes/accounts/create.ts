a1fb6ea8e7bd9ac1cc615d0167fba2e3d64e1d94
lanz0519 <1035305632@qq.com>
2022-08-03 17:33:40 +0800

    fix: change hashAPIKey path

--- packages/app-store/dbee_data/api/_accounts/routes/accounts/create.ts
+++ packages/app-store/dbee_data/api/_accounts/routes/accounts/create.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import prisma from "@calcom/prisma";
-import { hashAPIKey } from "@calcom/ee/lib/api/apiKeys";
+import { hashAPIKey } from "@calcom/features/ee/api-keys/lib/apiKeys";
+
 import { initDoctorCalSchedule, getRandomString } from "../../utils/index"
 import { Doctor } from '../../types/doctor'
 import { v4 as uuidv4 } from "uuid";
